1cf779ba056a3412d908313092c2c434969000df
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-07-17 13:34:54 -0300

    update dns again but to modify UTC

--- src/configuration/database/mysql/mysql_connection.go
+++ src/configuration/database/mysql/mysql_connection.go
@@ -3,6 +3,7 @@ package mysqldb
 import (
 	"database/sql"
 
+	"github.com/LaviqueDias/api-crud-go/src/configuration/logger"
 	_ "github.com/go-sql-driver/mysql"
 ) 
 
@@ -12,10 +13,14 @@ func Connection() (*sql.DB, error) {
 	
 	if db == nil {
 		var err error
-		db, err = sql.Open("mysql", "root:root@tcp(localhost:3307)/apicrudgo?charset=utf8mb4&parseTime=true&loc=Local")
+		db, err = sql.Open("mysql", "root:root@tcp(localhost:3307)/apicrudgo?charset=utf8mb4&parseTime=true&loc=America%2FSao_Paulo")
 		if err != nil {
 			return nil, err
 		}
+		if _, err := db.Exec("SET time_zone = '-03:00'"); err != nil {
+            // só um aviso, não trava a aplicação
+            logger.Error("warning: não foi possível ajustar time_zone da sessão: %v", err)
+        }
 	}
 	return db, nil
 }
