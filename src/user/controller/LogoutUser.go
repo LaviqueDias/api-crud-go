5e87650e400bd45de79ea389ff31281643cb38ea
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-07-30 12:48:47 -0300

    update LogoutUser> return a UserResponse

--- src/user/controller/LogoutUser.go
+++ src/user/controller/LogoutUser.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/LaviqueDias/api-crud-go/src/configuration/logger"
+	"github.com/LaviqueDias/api-crud-go/src/user/model"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -15,7 +16,7 @@ func (uc *userControllerInterface) LogoutUser(c *gin.Context) {
 
 	user, _ := c.Get("user")
 
-	logger.Info("User logged out", zap.Any("user", user))
+	logger.Info("User logged out", zap.Any("user", model.UserToUserResponse(user.(*model.User))), zap.String("journey", "logoutUser"))
 
 	// Invalida o cookie
 	c.SetCookie("Authorization", "", -1, "/", "", false, true)
